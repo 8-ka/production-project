655f098643757021f4a3360b71ecb350a711acb4
8_ka <breaka@bk.ru>
2023-08-25 16:35:39 +0300

    feat: routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 
-import { Test } from "./components/Test";
+import { Suspense } from 'react';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { MainPageLazy } from "./components/MainPage/MainPage.lazy";
+import { AboutPageLazy } from "./components/AboutPage/AboutPage.lazy";
 import './index.scss';
 
 const App = () => {
   return (
-    <div>
-        <Test />
-    </div>
+    <BrowserRouter>
+      <Link to='/'>Main page</Link>
+      <Link to='/about'>About page</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageLazy />} />
+          <Route path='/about' element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
