1a09e05f725ceb508374f3983711661e0a5e85b0
8_ka <breaka@bk.ru>
2023-09-15 16:33:24 +0300

    feat: add FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 
 import { Suspense, useContext, useState } from 'react';
 import { Link, Route, Routes } from "react-router-dom";
-import { MainPageLazy } from "./components/MainPage/MainPage.lazy";
-import { AboutPageLazy } from "./components/AboutPage/AboutPage.lazy";
-import { useTheme } from './theme/useTheme';
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import './styles/index.scss';
-import { classNames } from './helpers/classNames';
+import { MainPage } from 'pages/MainPage';
+import AboutPage from 'pages/AboutPage/ui/AboutPage';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
       <Link to='/about'>About page</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/' element={<MainPageLazy />} />
-          <Route path='/about' element={<AboutPageLazy />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
