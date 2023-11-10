75cb6cf5761969ff70ae4fa0ff7ec463c503490e
8_ka <breaka@bk.ru>
2023-11-10 14:45:44 +0300

    fix

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -7,6 +7,7 @@ describe('classNames', () => {
   
   test('with additional', () => {
     const expected = 'someClass class1 class2';
+    
     expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
   })
 })
